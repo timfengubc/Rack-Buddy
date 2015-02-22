b0913e458978f581a0b05e58f512865d440deaad
someHobo <xwei7778@gmail.com>
2015-02-22 11:54:21 -0800

    -Merged database branch with master branch -removed unused import

--- src/ubc/cs/cpsc310/rackbuddy/client/RackBuddy.java
+++ src/ubc/cs/cpsc310/rackbuddy/client/RackBuddy.java
@@ -3,24 +3,10 @@ package ubc.cs.cpsc310.rackbuddy.client;
 import java.util.ArrayList;
 import java.util.List;
 
-import ubc.cs.cpsc310.rackbuddy.shared.FieldVerifier;
-
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.event.dom.client.KeyCodes;
-import com.google.gwt.event.dom.client.KeyUpEvent;
-import com.google.gwt.event.dom.client.KeyUpHandler;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
-import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.DialogBox;
-import com.google.gwt.user.client.ui.HTML;
-import com.google.gwt.user.client.ui.Label;
-import com.google.gwt.user.client.ui.RootPanel;
-import com.google.gwt.user.client.ui.TextBox;
-import com.google.gwt.user.client.ui.VerticalPanel;
 
 public class RackBuddy implements EntryPoint {
 

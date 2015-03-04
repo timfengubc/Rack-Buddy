db0a96bb6c6c16d90a7659cd4088eebb5fafb0a9
Tim Feng <timfeng@msn.com>
2015-03-04 11:02:36 -0800

    Cleanup

--- src/ubc/cs/cpsc310/rackbuddy/client/RackBuddy.java
+++ src/ubc/cs/cpsc310/rackbuddy/client/RackBuddy.java
@@ -1,20 +1,16 @@
 package ubc.cs.cpsc310.rackbuddy.client;
 
-import java.util.List;
-
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.VerticalPanel;
-import com.google.gwt.user.client.Window;
 
 import com.google.gwt.user.client.ui.Anchor;
 
 public class RackBuddy implements EntryPoint {
 
-	private final JDOServiceAsync jdoRPC = GWT.create(JDOService.class);
 	private VerticalPanel mainPanel = new VerticalPanel();
 	private LoginInfo loginInfo = null;
 	private VerticalPanel loginPanel = new VerticalPanel();
@@ -64,122 +60,5 @@ public class RackBuddy implements EntryPoint {
 		
 		// Associate the Main panel with the HTML host page.
 		RootPanel.get("rackMap").add(mainPanel);
-	}
-	/**
-	 * Function to test get method Feel free to remove it
-	 */
-	private void get() {
-		jdoRPC.getAllData(new AsyncCallback<List<BikeRackData>>() {
-
-			@Override
-			public void onFailure(Throwable caught) {
-
-			}
-
-			@Override
-			public void onSuccess(List<BikeRackData> result) {
-
-			}
-
-		});
-	}
-
-	/**
-	 * Function to test add method Feel free to remove it
-	 */
-	private void add(List<BikeRackData> datas) {
-		for (BikeRackData data : datas) {
-			jdoRPC.addBikeRackData(data, new AsyncCallback<Void>() {
-
-				@Override
-				public void onFailure(Throwable caught) {
-
-				}
-
-				@Override
-				public void onSuccess(Void result) {
-					// Window.alert("successfully added");
-				}
-
-			});
-
-		}
-	}
-
-	/**
-	 * Function to test update method Feel free to remove it
-	 */
-	private void updateData(List<BikeRackData> result) {
-
-		for (BikeRackData data : result) {
-
-			BikeRackData temp = new BikeRackData(data);
-
-			temp.setBia("modified bia");
-			temp.setNumRacks(temp.getNumRacks() + 20);
-			temp.setSkytrainStation(" modified skytrain station");
-			temp.setStreetName(" modified street name");
-			temp.setStreetNumber(" updated street number");
-			temp.setStreetSide(" updated street side");
-			temp.setYearInstalled(" modified year installed");
-
-			jdoRPC.updateBikeRackData(data, temp, new AsyncCallback<Void>() {
-
-				@Override
-				public void onFailure(Throwable caught) {
-
-				}
-
-				@Override
-				public void onSuccess(Void result) {
-					Window.alert("updated");
-				}
-
-			});
-
-		}
-
-	}
-
-	/**
-	 * Function to test remove method Feel free to remove
-	 */
-	private void removeData(List<BikeRackData> toBeRemoved) {
-
-		for (BikeRackData data : toBeRemoved) {
-
-			jdoRPC.removeBikeRackData(data, new AsyncCallback<Void>() {
-
-				@Override
-				public void onFailure(Throwable caught) {
-
-				}
-
-				@Override
-				public void onSuccess(Void result) {
-					Window.alert("successfully removed");
-				}
-
-			});
-		}
-	}
-
-	/**
-	 * Function to test remove all method Feel free to remove
-	 */
-	private void removeAll() {
-		jdoRPC.removeAll(new AsyncCallback<Void>() {
-
-			@Override
-			public void onFailure(Throwable caught) {
-
-			}
-
-			@Override
-			public void onSuccess(Void result) {
-				Window.alert("successfully removed all");
-			}
-
-		});
 	}
 }

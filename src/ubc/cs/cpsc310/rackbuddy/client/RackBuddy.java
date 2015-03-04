0397d1c63fa1687a3566084e244a5785cbee7cad
Tim Feng <timfeng@msn.com>
2015-03-04 10:45:28 -0800

    Added Login functionality

--- src/ubc/cs/cpsc310/rackbuddy/client/RackBuddy.java
+++ src/ubc/cs/cpsc310/rackbuddy/client/RackBuddy.java
@@ -1,25 +1,83 @@
 package ubc.cs.cpsc310.rackbuddy.client;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.user.client.Window;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.FlexTable;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.event.dom.client.KeyCodes;
+import com.google.gwt.event.dom.client.KeyDownEvent;
+import com.google.gwt.event.dom.client.KeyDownHandler;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.HTML;
+
+import com.google.gwt.user.client.ui.Anchor;
 
 public class RackBuddy implements EntryPoint {
 
-	private JDOServiceAsync jdoRPC = GWT.create(JDOService.class);
+	private final JDOServiceAsync jdoRPC = GWT.create(JDOService.class);
+	private VerticalPanel mainPanel = new VerticalPanel();
+	private LoginInfo loginInfo = null;
+	private VerticalPanel loginPanel = new VerticalPanel();
+	private Label loginLabel = new Label(
+			"Please sign in to your Google Account to access the RackBuddy application.");
+	private Anchor signInLink = new Anchor("Sign In");
+	private Anchor signOutLink = new Anchor("Sign Out");
+
 	/**
 	 * This is the entry point method.
 	 */
 	public void onModuleLoad() {
+		// Check login status using login service.
+		LoginServiceAsync loginService = GWT.create(LoginService.class);
+		loginService.login(GWT.getHostPageBaseURL(),
+				new AsyncCallback<LoginInfo>() {
+					public void onFailure(Throwable error) {
+					}
+
+					public void onSuccess(LoginInfo result) {
+						loginInfo = result;
+						if (loginInfo.isLoggedIn()) {
+							loadRackBuddy();
+						} else {
+							loadLogin();
+						}
+					}
+				});
+		// Window.alert("loaded");
+	}
+	
+	private void loadLogin() {
+		// Assemble login panel.
+		signInLink.setHref(loginInfo.getLoginUrl());
+		loginPanel.add(loginLabel);
+		loginPanel.add(signInLink);
+		RootPanel.get("rackMap").add(loginPanel);
+	}
+
+	private void loadRackBuddy() {
+		signOutLink.setHref(loginInfo.getLogoutUrl());
+		/**
+		 * To be implemented
+		 */
 		
-		//Window.alert("loaded");
+		mainPanel.add(signOutLink);
+		
+		// Associate the Main panel with the HTML host page.
+		RootPanel.get("rackMap").add(mainPanel);
 	}
 	/**
-	 * Function to test get method
-	 * Feel free to remove it
+	 * Function to test get method Feel free to remove it
 	 */
 	private void get() {
 		jdoRPC.getAllData(new AsyncCallback<List<BikeRackData>>() {
@@ -36,10 +94,9 @@ public class RackBuddy implements EntryPoint {
 
 		});
 	}
-	
+
 	/**
-	 * Function to test add method
-	 * Feel free to remove it
+	 * Function to test add method Feel free to remove it
 	 */
 	private void add(List<BikeRackData> datas) {
 		for (BikeRackData data : datas) {
@@ -59,9 +116,9 @@ public class RackBuddy implements EntryPoint {
 
 		}
 	}
+
 	/**
-	 * Function to test update method
-	 * Feel free to remove it
+	 * Function to test update method Feel free to remove it
 	 */
 	private void updateData(List<BikeRackData> result) {
 
@@ -94,46 +151,46 @@ public class RackBuddy implements EntryPoint {
 		}
 
 	}
+
 	/**
-	 * Function to test remove method
-	 * Feel free to remove
+	 * Function to test remove method Feel free to remove
 	 */
-	private void removeData(List<BikeRackData> toBeRemoved){
-		
-		for(BikeRackData data : toBeRemoved){
-			
-			jdoRPC.removeBikeRackData(data, new AsyncCallback<Void>(){
+	private void removeData(List<BikeRackData> toBeRemoved) {
+
+		for (BikeRackData data : toBeRemoved) {
+
+			jdoRPC.removeBikeRackData(data, new AsyncCallback<Void>() {
 
 				@Override
 				public void onFailure(Throwable caught) {
-					
+
 				}
 
 				@Override
 				public void onSuccess(Void result) {
 					Window.alert("successfully removed");
 				}
-				
+
 			});
 		}
 	}
+
 	/**
-	 * Function to test remove all method
-	 * Feel free to remove
+	 * Function to test remove all method Feel free to remove
 	 */
-	private void removeAll(){
-		jdoRPC.removeAll(new AsyncCallback<Void>(){
+	private void removeAll() {
+		jdoRPC.removeAll(new AsyncCallback<Void>() {
 
 			@Override
 			public void onFailure(Throwable caught) {
-				
+
 			}
 
 			@Override
 			public void onSuccess(Void result) {
 				Window.alert("successfully removed all");
 			}
-			
+
 		});
 	}
 }

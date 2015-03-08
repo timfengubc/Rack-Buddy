e836a1c464dc53619dcc9960b5cc1116acfad48f
someHobo <xwei7778@gmail.com>
2015-03-07 21:32:51 -0800

    -Created widgets to allow searching of bike rack locations from a specific poi -removed unused imports -reformatted some comments

--- src/ubc/cs/cpsc310/rackbuddy/client/RackBuddy.java
+++ src/ubc/cs/cpsc310/rackbuddy/client/RackBuddy.java
@@ -1,17 +1,12 @@
 package ubc.cs.cpsc310.rackbuddy.client;
 
-import java.util.ArrayList;
-import java.util.List;
 
-import ubc.cs.cpsc310.rackbuddy.server.GeoParser;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.Window;
-import com.google.gwt.dom.client.Style.Unit;
-import com.google.gwt.maps.client.InfoWindowContent;
 import com.google.gwt.maps.client.MapWidget;
 import com.google.gwt.maps.client.Maps;
 import com.google.gwt.maps.client.control.LargeMapControl;
@@ -20,16 +15,14 @@ import com.google.gwt.maps.client.overlay.Marker;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.FlowPanel;
-import com.google.gwt.user.client.ui.DockLayoutPanel;
-import com.google.gwt.user.client.ui.HTML;
-import com.google.gwt.user.client.ui.HTMLPanel;
+import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.LayoutPanel;
-import com.google.gwt.user.client.ui.RootLayoutPanel;
+import com.google.gwt.user.client.ui.ListBox;
 import com.google.gwt.user.client.ui.RootPanel;
-import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.Anchor;
-import com.google.gwt.user.client.ui.Widget;
+import com.google.gwt.user.client.ui.VerticalPanel;
 
 public class RackBuddy implements EntryPoint {
 	
@@ -41,9 +34,12 @@ public class RackBuddy implements EntryPoint {
 	private Anchor signInLink = new Anchor("Sign In");
 	private String signOutLink = new String();
 	private Button signOutButton = new Button("Sign Out");
-
-	private GeoParserServiceAsync service = GWT.create(GeoParserService.class);
-	List<Double> coords = new ArrayList<Double>();
+	private MapWidget map;
+	
+	private VerticalPanel searchPanel;
+	private Button searchButton;
+	private TextBox address;
+	private ListBox searchRadius;
 
 	/**
 	 * This is the entry point method.
@@ -65,11 +61,11 @@ public class RackBuddy implements EntryPoint {
 						}
 					}
 				});
-
 	}
-
+	/**
+	 * Assemble login panel
+	 */
 	private void loadLogin() {
-		// Assemble login panel.
 		signInLink.setHref(loginInfo.getLoginUrl());
 		loginPanel.add(loginLabel);
 		loginPanel.add(signInLink);
@@ -81,13 +77,12 @@ public class RackBuddy implements EntryPoint {
 		Maps.loadMapsApi("", "2", false, new Runnable(){
 			public void run() {
 				buildUi();
+
 			}
 		});
-		
 		signOutLink = loginInfo.getLogoutUrl();
-		/**
-		 * To be implemented
-		 */
+		
+		//TODO: to be implemented
 
 		// Associate the panels with the HTML host page.
 		RootPanel.get("signOut").add(signOutButton);
@@ -97,9 +92,9 @@ public class RackBuddy implements EntryPoint {
 			public void onClick(ClickEvent event) {
 				Window.Location.assign(signOutLink);
 			}
-
 		});
-
+		
+		
 		RootPanel.get("loadData").add(loadData);
 		if (loginInfo.getAdmin() == false) {
 			loadData.setVisible(false);
@@ -109,27 +104,86 @@ public class RackBuddy implements EntryPoint {
 	}
 
 	private void buildUi() {
-		// Open a map centered on Cawker City, KS USA
-		 LatLng vancouver = LatLng.newInstance(49.261226,-123.1139268);
-		// LatLng ponderosa = LatLng.newInstance(coords.get(0), coords.get(1));
-
-		final MapWidget map = new MapWidget(vancouver, 2);
+		LatLng vancouver = LatLng.newInstance(49.261226,-123.1139268);
+		map = new MapWidget(vancouver, 2);		
 		map.setSize("60%", "100%");
 		map.setZoomLevel(12);
-		// Add some controls for the zoom level
 		map.addControl(new LargeMapControl());
-
-		// Add a marker
-		 map.addOverlay(new Marker(vancouver));
-		// map.addOverlay(new Marker(ponderosa));
-
-		// Add an info window to highlight a point of interest
-		map.getInfoWindow().open(vancouver, new InfoWindowContent("Vancouver"));
-
+		map.addOverlay(new Marker(vancouver));
+	
 		LayoutPanel rackMapPanel = new LayoutPanel();
 		rackMapPanel.setSize("100em", "40em");
 		rackMapPanel.setStyleName("rackMap");
 		rackMapPanel.add(map);
+		
 		RootPanel.get().add(rackMapPanel);
+		
+		initSearchPanel();
 	}
+
+	private void initSearchPanel() {
+		searchPanel = new VerticalPanel();
+		
+		searchButton = new Button("Search...");
+		searchButton.addStyleName("marginTop");
+		
+		searchButton.addClickHandler(new ClickHandler(){
+
+			@Override
+			public void onClick(ClickEvent event) {
+				if(isTextValid(address.getValue()) == true){
+					//TODO retrieve results
+				}
+				
+			}
+			
+		});
+		
+		address = new TextBox();
+		address.addStyleName("paddedLeft");
+		address.setWidth("40em");
+		address.setHeight("10px");
+
+		searchRadius = new ListBox();
+		searchRadius.setVisibleItemCount(1);
+		searchRadius.addItem("100 m");
+		searchRadius.addItem("500 m");
+		searchRadius.addItem("1 km");
+		
+		HorizontalPanel h1 = new HorizontalPanel();
+		h1.setStyleName("marginTop");
+		
+		HorizontalPanel h2 = new HorizontalPanel();
+		h2.setStyleName("marginTop");
+		
+		h1.add(new Label("Address to search from:"));
+		h1.add(address);
+		
+		h2.add(new Label("Search radius: "));
+		h2.add(searchRadius);
+		
+		searchPanel.add(h1);
+		searchPanel.add(h2);
+		searchPanel.add(searchButton);
+		
+		RootPanel.get().add(searchPanel);
+	}
+	
+	private boolean isTextValid(String text){
+		
+		if(text == null){
+			return false;
+		}
+		
+		if(text.isEmpty()){
+			return false;
+		}
+		
+		if(text.matches("\\s+")){
+			return false;
+		}
+		
+		return true;
+	}
+	
 }

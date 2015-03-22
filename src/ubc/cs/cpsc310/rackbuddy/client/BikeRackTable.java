c70f93b3a5827c1dfc0cc18ae9e487acc4987668
AnthonyChen7 <xwei7778@gmail.com>
2015-03-21 19:02:51 -0700

    Able to display Bike rack data in official table changed to display only 10 items at a time in official table

--- src/ubc/cs/cpsc310/rackbuddy/client/BikeRackTable.java
+++ src/ubc/cs/cpsc310/rackbuddy/client/BikeRackTable.java
@@ -1,14 +1,12 @@
 package ubc.cs.cpsc310.rackbuddy.client;
 
-import java.util.Arrays;
-import java.util.Date;
 import java.util.List;
 
-import com.google.gwt.cell.client.DateCell;
+import com.google.gwt.core.shared.GWT;
 import com.google.gwt.user.cellview.client.CellTable;
-import com.google.gwt.user.cellview.client.Column;
 import com.google.gwt.user.cellview.client.SimplePager;
 import com.google.gwt.user.cellview.client.TextColumn;
+import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.IsWidget;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.VerticalPanel;
@@ -18,46 +16,30 @@ import com.google.gwt.view.client.HasData;
 
 public class BikeRackTable implements IsWidget {
 
-	private static final String BIKE_RACK_LOCATIONS_IN_THE_CITY_OF_VANCOUVER = "Official Bike Rack Locations in the City of Vancouver";
+	public static final String YEARS_INSTALLED = "Years Installed";
 
+	public static final String NUM_RACKS = "# of Racks";
 
+	public static final String BIA2 = "BIA";
 
-	/**
-	 * A simple data type that represents a contact.
-	 */
-	private static class Contact {
-		private final String address;
-		private final Date birthday;
-		private final String name;
+	public static final String SKYTRAIN_STATION_NAME = "Skytrain Station Name";
 
-		public Contact(String name, Date birthday, String address) {
-			this.name = name;
-			this.birthday = birthday;
-			this.address = address;
-		}
-	}
+	public static final String ST_SIDE = "St. Side";
 
-	/**
-	 * The list of data to display.
-	 */
-	@SuppressWarnings("deprecation")
-	private static final List<Contact> CONTACTS = Arrays.asList(new Contact(
-			"John", new Date(80, 4, 12), "123 Abc Avenue"), new Contact("Joe",
-			new Date(85, 2, 22), "22 Lance Ln"), new Contact("Tom", new Date(
-			85, 3, 22), "33 Lance Ln"), new Contact("Jack",
-			new Date(85, 4, 22), "44 Lance Ln"), new Contact("Tim", new Date(
-			85, 5, 22), "55 Lance Ln"), new Contact("Mike",
-			new Date(85, 6, 22), "66 Lance Ln"), new Contact("George",
-			new Date(46, 6, 6), "77 Lance Ln"));
+	public static final String ST_NAME = "St. Name";
 
+	public static final String ST_NUMBER = "St. Number";
+
+	public static final String BIKE_RACK_LOCATIONS_IN_THE_CITY_OF_VANCOUVER = "Official Bike Rack Locations in the City of Vancouver";
 	
+	private JDOServiceAsync jdoService = GWT.create(JDOService.class);
 
 	@Override
 	public Widget asWidget() {
 				// Create a CellTable.
 				final CellTable<BikeRackData> table = new CellTable<BikeRackData>();
 				
-				table.setPageSize(20);
+				table.setPageSize(10);
 
 				// Add a text column to show the street num.
 				TextColumn<BikeRackData> stNum = new TextColumn<BikeRackData>() {
@@ -66,17 +48,7 @@ public class BikeRackTable implements IsWidget {
 						return object.getStreetNumber();
 					}
 				};
-				table.addColumn(stNum, "St. Number");
-
-				// Add a date column to show the birthday.
-//				DateCell dateCell = new DateCell();
-//				Column<Contact, Date> dateColumn = new Column<Contact, Date>(dateCell) {
-//					@Override
-//					public Date getValue(Contact object) {
-//						return object.birthday;
-//					}
-//				};
-//				table.addColumn(dateColumn, "Birthday");
+				table.addColumn(stNum, ST_NUMBER);
 
 				// Add a text column to show the street name.
 				TextColumn<BikeRackData> stName = new TextColumn<BikeRackData>() {
@@ -85,7 +57,7 @@ public class BikeRackTable implements IsWidget {
 						return object.getStreetName();
 					}
 				};
-				table.addColumn(stName, "St. Name");
+				table.addColumn(stName, ST_NAME);
 				
 				TextColumn<BikeRackData> stSide = new TextColumn<BikeRackData>() {
 					@Override
@@ -93,7 +65,7 @@ public class BikeRackTable implements IsWidget {
 						return object.getStreetName();
 					}
 				};
-				table.addColumn(stSide, "St. Side");
+				table.addColumn(stSide, ST_SIDE);
 				
 				TextColumn<BikeRackData> skytrainStn = new TextColumn<BikeRackData>() {
 					@Override
@@ -101,7 +73,7 @@ public class BikeRackTable implements IsWidget {
 						return object.getSkytrainStation();
 					}
 				};
-				table.addColumn(skytrainStn, "Skytrain Station Name");
+				table.addColumn(skytrainStn, SKYTRAIN_STATION_NAME);
 				
 				TextColumn<BikeRackData> bia = new TextColumn<BikeRackData>() {
 					@Override
@@ -109,7 +81,7 @@ public class BikeRackTable implements IsWidget {
 						return object.getBia();
 					}
 				};
-				table.addColumn(bia, "BIA");
+				table.addColumn(bia, BIA2);
 				
 				TextColumn<BikeRackData> numRacks = new TextColumn<BikeRackData>() {
 					@Override
@@ -117,7 +89,7 @@ public class BikeRackTable implements IsWidget {
 						return String.valueOf(object.getNumRacks());
 					}
 				};
-				table.addColumn(numRacks, "# of Racks");
+				table.addColumn(numRacks, NUM_RACKS);
 				
 				TextColumn<BikeRackData> yearsInstalled = new TextColumn<BikeRackData>() {
 					@Override
@@ -125,23 +97,32 @@ public class BikeRackTable implements IsWidget {
 						return object.getYearInstalled();
 					}
 				};
-				table.addColumn(yearsInstalled, "Years Installed");
+				table.addColumn(yearsInstalled, YEARS_INSTALLED);
+				
+				jdoService.getAllData(new AsyncCallback<List<BikeRackData>>(){
 
-				// Associate an async data provider to the table
-				// XXX: Use AsyncCallback in the method onRangeChanged
-				// to actaully get the data from the server side
-//				AsyncDataProvider<Contact> provider = new AsyncDataProvider<Contact>() {
-//					@Override
-//					protected void onRangeChanged(HasData<Contact> display) {
-//						int start = display.getVisibleRange().getStart();
-//						int end = start + display.getVisibleRange().getLength();
-//						end = end >= CONTACTS.size() ? CONTACTS.size() : end;
-//						List<Contact> sub = CONTACTS.subList(start, end);
-//						updateRowData(start, sub);
-//					}
-//				};
-//				provider.addDataDisplay(table);
-//				provider.updateRowCount(CONTACTS.size(), true);
+					@Override
+					public void onFailure(Throwable caught) {
+						
+					}
+
+					@Override
+					public void onSuccess(final List<BikeRackData> result) {
+						AsyncDataProvider<BikeRackData> provider = new AsyncDataProvider<BikeRackData>() {
+						@Override
+						protected void onRangeChanged(HasData<BikeRackData> display) {
+							int start = display.getVisibleRange().getStart();
+							int end = start + display.getVisibleRange().getLength();
+							end = end >= result.size() ? result.size() : end;
+							List<BikeRackData> sub = result.subList(start, end);
+							updateRowData(start, sub);
+						}
+					};
+					provider.addDataDisplay(table);
+					provider.updateRowCount(result.size(), true);
+					}
+					
+				});
 
 				SimplePager pager = new SimplePager();
 				pager.setDisplay(table);

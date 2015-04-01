cb2a0da127da0d6b47dea952f9ba6739a92d8035
Tim <timfeng@msn.com>
2015-03-31 19:58:07 -0700

    making everything look nice

--- src/ubc/cs/cpsc310/rackbuddy/client/RackBuddy.java
+++ src/ubc/cs/cpsc310/rackbuddy/client/RackBuddy.java
@@ -33,10 +33,10 @@ public class RackBuddy implements EntryPoint {
 	private Button signOutButton = new Button("Sign Out");
 	private JDOServiceAsync jdoService = GWT.create(JDOService.class);
 	private MapDisplay mapDisplay;
-	
+
 	private VerticalPanel adminPanel = new VerticalPanel();
 	private Button loadData = new Button("Load Data");
-	private Button removeData = new Button("Remove all Data");
+	private Button removeData = new Button("Remove Data");
 	private String url = "http://m.uploadedit.com/ba3a/1426016101419.txt";
 	private TextBox urlbox = new TextBox();
 	private ListBox yrListBox = new ListBox();
@@ -46,9 +46,7 @@ public class RackBuddy implements EntryPoint {
 	private String _2012 = "2012";
 	private String _2013 = "2013";
 	private String _2014 = "2014";
-	
-	
-	
+
 	/**
 	 * This is the entry point method.
 	 */
@@ -63,16 +61,16 @@ public class RackBuddy implements EntryPoint {
 
 					public void onSuccess(LoginInfo result) {
 						loginInfo = result;
-						if (loginInfo.isLoggedIn()) {	
+						if (loginInfo.isLoggedIn()) {
 							loadRackBuddy();
-						
-							
+
 						} else {
 							loadLogin();
 						}
 					}
 				});
 	}
+
 	/**
 	 * Assemble login panel
 	 */
@@ -83,73 +81,74 @@ public class RackBuddy implements EntryPoint {
 		RootPanel.get("rackMap").add(loginPanel);
 
 	}
-	
+
 	private void loadRackBuddy() {
 
 		mapDisplay = new MapDisplay(loginInfo);
-		
-//		BikeRackTable bikeRackTable  = new BikeRackTable(loginInfo);
-//		FavRackTable favRackTable = new FavRackTable(loginInfo);
-//		
-//		VerticalPanel bigTable = new VerticalPanel();
-//		
-//		final TabPanel p = new TabPanel();
-//	    p.add(bikeRackTable, BikeRackTable.BIKE_RACK_LOCATIONS_IN_THE_CITY_OF_VANCOUVER, false);
-//	    p.add(favRackTable, FavRackTable.USER_S_FAVORITE_BIKE_RACK_LOCATION, false);
-//
-//	    p.selectTab(0);
-//	    
-//	    bigTable.add(p);
-//	    
-//	    RootPanel.get("bigTable").add(bigTable);
+
+		// BikeRackTable bikeRackTable = new BikeRackTable(loginInfo);
+		// FavRackTable favRackTable = new FavRackTable(loginInfo);
+		//
+		// VerticalPanel bigTable = new VerticalPanel();
+		//
+		// final TabPanel p = new TabPanel();
+		// p.add(bikeRackTable,
+		// BikeRackTable.BIKE_RACK_LOCATIONS_IN_THE_CITY_OF_VANCOUVER, false);
+		// p.add(favRackTable, FavRackTable.USER_S_FAVORITE_BIKE_RACK_LOCATION,
+		// false);
+		//
+		// p.selectTab(0);
+		//
+		// bigTable.add(p);
+		//
+		// RootPanel.get("bigTable").add(bigTable);
 
 		RootPanel.get("rackMap").add(mapDisplay.getMapPanel());
-		
+
 		signOutLink = loginInfo.getLogoutUrl();
 
 		// Associate the panels with the HTML host page.
 		RootPanel.get("signOut").add(signOutButton);
-  
-		//admin rights check
-			if (loginInfo.getAdmin() == false) {
-				adminPanel.setVisible(false);
-			} else {
-				adminPanel.setVisible(true);
-			}
-			
-		  
+
+		// admin rights check
+		if (loginInfo.getAdmin() == false) {
+			adminPanel.setVisible(false);
+		} else {
+			adminPanel.setVisible(true);
+		}
+
 		setHandlers();
 		initAdminPanel();
 
-		
-		  
-		/*	refreshDispl.addClickHandler(new ClickHandler(){
-
-				@Override
-				public void onClick(ClickEvent event) {
-					mapDisplay.displayAllMarkers();
-				}
-				});*/
+		/*
+		 * refreshDispl.addClickHandler(new ClickHandler(){
+		 * 
+		 * @Override public void onClick(ClickEvent event) {
+		 * mapDisplay.displayAllMarkers(); } });
+		 */
 
 	}
 
 	private void loadRacks() {
-		 jdoService.loadRacks(urlbox.getValue(), yrListBox.getValue(yrListBox.getSelectedIndex()), new AsyncCallback<Void>() {
-		    	public void onFailure(Throwable error) {
-			    	 handleError(error);
-			      }
-		      public void onSuccess(Void ignore) {
-		    	    Window.alert(DATA_LOADED);
-		    	    mapDisplay.displayAllMarkers();
-		    	  	
-		      }	
+		jdoService.loadRacks(urlbox.getValue(),
+				yrListBox.getValue(yrListBox.getSelectedIndex()),
+				new AsyncCallback<Void>() {
+					public void onFailure(Throwable error) {
+						handleError(error);
+					}
 
-		    });
-		
+					public void onSuccess(Void ignore) {
+						Window.alert(DATA_LOADED);
+						mapDisplay.displayAllMarkers();
+
+					}
+
+				});
+
 	}
-	
-	private void removeAllData(){
-		jdoService.removeAll(new AsyncCallback<Void>(){
+
+	private void removeAllData() {
+		jdoService.removeAll(new AsyncCallback<Void>() {
 
 			@Override
 			public void onFailure(Throwable caught) {
@@ -157,89 +156,91 @@ public class RackBuddy implements EntryPoint {
 			}
 
 			@Override
-			public void onSuccess(Void result) {				
+			public void onSuccess(Void result) {
 				Window.alert(ALL_DATA_SUCCESSFULLY_REMOVED_FROM_DATASTORE);
-				  mapDisplay.displayAllMarkers();
+				mapDisplay.displayAllMarkers();
 			}
-			
+
 		});
 	}
-	
-	
-		
+
 	private void handleError(Throwable error) {
-	    Window.alert(error.getMessage());
-	
-	    }
-	
-    private void initAdminPanel() {
-    	 //load data panel formatting
-		 HorizontalPanel loadPanel = new HorizontalPanel();
-		
-		urlbox.addStyleName("paddedRight");
+		Window.alert(error.getMessage());
+
+	}
+
+	private void initAdminPanel() {
+		// load data panel formatting
+		HorizontalPanel loadPanel = new HorizontalPanel();
+
 		urlbox.setWidth("20em");
-		urlbox.setHeight("10px");
+		urlbox.setHeight("1em");
 		urlbox.setValue(url);
-		
+
 		yrListBox.setVisibleItemCount(1);
 		yrListBox.addItem(p2010);
 		yrListBox.addItem(_2011);
 		yrListBox.addItem(_2012);
 		yrListBox.addItem(_2013);
 		yrListBox.addItem(_2014);
+		yrListBox.setHeight("2em");
+		yrListBox.setWidth("7em");
 		
-		loadPanel.setStyleName("marginTop");
+		loadData.setHeight("2em");
+		loadData.setWidth("6em");
+		
+		removeData.setHeight("2em");
+		removeData.setWidth("8em");
+		
 		loadPanel.add(yrListBox);
 		loadPanel.add(loadData);
+		loadPanel.add(removeData);
 		
 		adminPanel.add(new Label(LOAD_FROM));
 		adminPanel.add(urlbox);
 		adminPanel.add(loadPanel);
-		adminPanel.add(removeData);
-	
-		
-		
+
 		RootPanel.get("loadData").add(adminPanel);
-		
-    }
-    
+
+	}
+
 	private void setHandlers() {
-		
+
 		signOutButton.addClickHandler(new ClickHandler() {
 			@Override
 			public void onClick(ClickEvent event) {
 				Window.Location.assign(signOutLink);
 			}
 		});
-	
-		//listen on Load Rack Data Button
-		  loadData.addClickHandler(new ClickHandler() {
-			  @Override
-		      public void onClick(ClickEvent event) {
-		        loadRacks();
-		      }
-		    });
-		  
-		  //listen on removealldatabutton
-		  removeData.addClickHandler(new ClickHandler(){
+
+		// listen on Load Rack Data Button
+		loadData.addClickHandler(new ClickHandler() {
+			@Override
+			public void onClick(ClickEvent event) {
+				loadRacks();
+			}
+		});
+
+		// listen on removealldatabutton
+		removeData.addClickHandler(new ClickHandler() {
 
 			@Override
 			public void onClick(ClickEvent event) {
 				removeAllData();
-				
+
 			}
-			});
+		});
 	}
-	
-	public static native void shareFb() /*-{ 
+
+	public static native void shareFb() /*-{
 		$wnd.FB.ui({
-  method: 'share_open_graph',
-  action_type: 'og.likes',
-  action_properties: JSON.stringify({
-      object:'http://1-dot-wxcs310s1final.appspot.com/',
-  })
-}, function(response){});
-	
+			method : 'share_open_graph',
+			action_type : 'og.likes',
+			action_properties : JSON.stringify({
+				object : 'http://1-dot-wxcs310s1final.appspot.com/',
+			})
+		}, function(response) {
+		});
+
 	}-*/;
 }
-
